652d92b091a1a80e3ea67b5650a29d85bbfa9657
VladNaboka <naboka.wlad@mail.ru>
2023-04-12 21:47:22 +0600

    Fixed camera bug

--- Assets/Scripts/Controllers/CameraController.cs
+++ Assets/Scripts/Controllers/CameraController.cs
@@ -7,12 +7,11 @@ public class CameraController : MonoBehaviour
     //Контроллер камеры
     [SerializeField] public float speedMouse = 300f;
     float xRot = 0f;
-    public Transform player;
+    float yRot = 0f;
     float mouseX;
     float mouseY;
     private void Start()
     {
-        //player = GameObject.Find("Player").transform;
         Cursor.lockState = CursorLockMode.Locked;
     }
     public void Update()
@@ -21,10 +20,10 @@ public class CameraController : MonoBehaviour
         mouseX = Input.GetAxis("Mouse X") * speedMouse * Time.deltaTime;
         mouseY = Input.GetAxis("Mouse Y") * speedMouse * Time.deltaTime;
         xRot -= mouseY;
-        xRot = Mathf.Clamp(xRot, -70f, 70f);
-        transform.localRotation = Quaternion.Euler(xRot, 0f, 0f);
-
-        player.Rotate(0, mouseX, 0);
+        yRot += mouseX;
+        xRot = Mathf.Clamp(xRot, -40f, 40f);
+        yRot = Mathf.Clamp(yRot, -40f, 40f);
+        transform.localRotation = Quaternion.Euler(xRot, yRot, 0f);
         //Приближение камеры по колесику мышки
         Camera.main.fieldOfView -= Input.GetAxis("Mouse ScrollWheel") * 100;
         Camera.main.fieldOfView = Mathf.Clamp(Camera.main.fieldOfView, 20, 60);
